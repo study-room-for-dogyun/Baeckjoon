dff1ab25c9834d8cf97f23f73c413190e15320fc
dogyun <kdk124578@naver.com>
2021-10-07 22:12:04 +0900

    21/10/07

---
+++ Implement/15717.py
@@ -0,0 +1,5 @@
+# https://www.acmicpc.net/problem/15717
+
+n = int(input())
+
+print((2**(n-1)))
--- code.plus/문제 - 시뮬레이션/14503.py
+++ code.plus/문제 - 시뮬레이션/14503.py
@@ -1,66 +1,45 @@
-from collections import deque
+# https://www.acmicpc.net/problem/14503
 
 n, m = map(int, input().split())
-x, y, d = map(int, input().split())
+r, c, d = map(int, input().split())
+
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-# 0  1  2  3 
 # 북 동 남 서
-# <-- 반시계방향 회전
-dx = [0, 1, 0, -1]
-dy = [-1, 0, 1, 0]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-def bfs():
-    global d
-    result = 0
-    q = deque([[x, y]])
-    graph[x][y] = 2
+answer = 0
 
-    while q:
+# 0: 청소 안 한 공간
+# 1: 벽
+# 2: 청소 한 공간
+# 현재 방향 d 기준 -1 왼쪽, +2하면 뒤쪽임.
+# 방향 바꿀 때 : d 값 수정
+# 이동만 할 때 : r, c 값에 d값 더해서 수정
+# 그러니까 후진 할 때는 d값은 업데이트 하면 안 됨.
+while True:
 
-        cx, cy = q.popleft()
+    if graph[r][c] == 0:
+        graph[r][c] = 2
+        answer += 1
 
-        # 네 방향 모두 청소나 벽이면 (벽 1, 청소 2)
-        cnt = 0
-        for i in range(4):
-            tx = cx + dx[i]
-            ty = cy + dy[i]
-            if not(0 <= tx < n and 0 <= ty < m):
-                cnt += 1
-                continue
-
-            if graph[tx][ty] == 1 or graph[tx][ty] == 2:
-                cnt += 1
-        
-        if cnt == 4:
-            l = d + 2
-            if l > 3:
-                l %= 2
-            if graph[cx + dx[l]][cy + dy[l]] == 1:
-                return result
-            q.append([cx + dx[l], cy + dy[l]])
-            continue
-
-        # 왼쪽으로 회전
+    check = 0
+    for _ in range(4):
         d -= 1
-        if d < 0:
-            d = 3
-
-        nx = cx + dx[d]
-        ny = cy + dy[d]
-
-        # 범위 벗어나면
-        if not(0 <= nx < n and 0 <= ny < m):
-            continue
-
-        # 왼쪽에 청소할 게 있으면
-        if graph[nx][ny] == 0:
-            graph[nx][ny] = 2
-            result += 1
-            q.append([nx, ny])
-            continue
-
+        next_position = graph[r + dx[d%4]][c + dy[d%4]]
+        if next_position == 0:
+            r += dx[d%4]
+            c += dy[d%4]
+            break
+        check += 1
     
-    return result
+    if check == 4:
+        back_position = graph[r + dx[(d+2)%4]][c + dy[(d+2)%4]]
+        if back_position == 1:
+            break
+        else:
+            r += dx[(d+2)%4]
+            c += dy[(d+2)%4]
 
-print(bfs())
+print(answer)
