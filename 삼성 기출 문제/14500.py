1d63eed550efbdcca2966d7f7dc4f47ba7aedf75
dogyun <kdk124578@naver.com>
2021-10-11 23:57:11 +0900

    21/10/11

--- 삼성 기출 문제/14500.py
+++ 삼성 기출 문제/14500.py
@@ -1,26 +1,51 @@
-# https://www.acmicpc.net/problem/14500
-
-# INPUT
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
+maxi = 0
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-# Solution
+def dfs(value, depth, x, y):
+    global maxi, visited
 
-# 1. 폴리오미노 형태 정의
-x = 0
-y = 0
+    if depth == 4:
+        maxi = max(maxi, value)
+        return
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-def a(x, y):
-    return [[x, y], [x+1, y], [x+2, y], [x+3, y]]
-def b(x, y):
-    return [[x, y], [x+1, y], [x, y+1], [x+1, y+1]]
-def c(x, y):
-    return[[x, y], [x, y+1], [x, y+2], [x+1, y+2]]
-def d(x, y):
-    return [[x, y], [x, y+1], [x+1, y+1], [x+1, y+2]]
-def e(x, y):
-    return [[x, y], [x+1, y], [x+2, y] ,[x+1, y+1]]
+        if not(0 <= nx < n and 0 <= ny < m) or visited[nx][ny]:
+            continue
+        
+        visited[nx][ny] = True
+        dfs(value+graph[nx][ny], depth+1, nx, ny)
+        visited[nx][ny] = False
 
+def fuck(x, y):
+    global maxi
 
-print(a(2, 3))
+    # ㅗ
+    if 1 <= x < n-1 and 1 <= y < m:
+        maxi = max(maxi, graph[x-1][y]+graph[x][y]+graph[x+1][y]+graph[x][y-1])
+    # ㅓ
+    if 1 <= x < n and 1 <= y < m-1:
+        maxi = max(maxi, graph[x-1][y]+graph[x][y]+graph[x][y-1]+graph[x][y+1])
+    # ㅏ
+    if 0 <= x < n-1 and 1 <= y < m-1:
+        maxi = max(maxi, graph[x][y]+graph[x][y-1]+graph[x][y+1]+graph[x+1][y])
+    # ㅜ
+    if 1 <= x < n-1 and 0 <= y < m-1:
+        maxi = max(maxi, graph[x][y]+graph[x-1][y]+graph[x+1][y]+graph[x][y+1])
+
+for i in range(n):
+    for j in range(m):
+        visited[i][j] = True
+        dfs(graph[i][j], 1, i, j)
+        visited[i][j] = False
+
+        fuck(i, j)
+
+print(maxi)
