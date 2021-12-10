3b74371ed3d51f9bbe505ca924b21b312607b77d
dogyun <kdk124578@naver.com>
2021-12-10 22:02:37 +0900

    21/12/10

--- code.plus/기초 - 그래프와 BFS/13549.py
+++ code.plus/기초 - 그래프와 BFS/13549.py
@@ -10,16 +10,17 @@ visited[n] = 0
 
 while q:
     cur = q.popleft()
-    if 2*cur < MAX and visited[2*cur] == -1:
-        q.appendleft(2*cur)
-        visited[2*cur] = visited[cur]
-        
-    if 0 <= cur - 1 and visited[cur-1] == -1:
-        q.append(cur-1)
-        visited[cur-1] = visited[cur] + 1
 
-    if cur + 1 < MAX and visited[cur+1] == -1:
-        q.append(cur+1)
-        visited[cur+1] = visited[cur] + 1
+    if 2 * cur < MAX and visited[2 * cur] == -1:
+        q.appendleft(2 * cur)
+        visited[2 * cur] = visited[cur]
+
+    if 0 <= cur - 1 and visited[cur - 1] == -1:
+        q.append(cur - 1)
+        visited[cur - 1] = visited[cur] + 1
+
+    if cur + 1 < MAX and visited[cur + 1] == -1:
+        q.append(cur + 1)
+        visited[cur + 1] = visited[cur] + 1
 
 print(visited[k])
--- code.plus/기초 - 그래프와 BFS/13913.py
+++ code.plus/기초 - 그래프와 BFS/13913.py
@@ -4,22 +4,35 @@ from collections import deque
 
 N, K = map(int, input().split())
 dp = [0] * 100001
+move = [0] * 100001
 
-q = deque([N])
-while q:
-    cur = q.popleft()
 
-    if cur + 1 < 100001:
-        if dp[cur + 1] == 0 or dp[cur + 1] > dp[cur] + 1:
-            q.append(cur + 1)
-            dp[cur + 1] = dp[cur] + 1
-    if cur - 1 > 0:
-        if dp[cur - 1] == 0 or dp[cur - 1] > dp[cur] + 1:
-            q.append(cur - 1)
-            dp[cur - 1] = dp[cur] + 1
-    if 2 * cur < 100001:
-        if dp[2 * cur] == 0 or dp[2 * cur] > dp[cur] + 1:
-            q.append(2 * cur)
-            dp[2 * cur] = dp[cur] + 1
+def path(x):
+    arr = []
+    temp = x
+    for _ in range(dp[x] + 1):
+        arr.append(temp)
+        temp = move[temp]
 
-print(dp[17])
+    print(' '.join(map(str, arr[::-1])))
+
+
+def bfs():
+    q = deque([N])
+
+    while q:
+        cur = q.popleft()
+
+        if cur == K:
+            print(dp[cur])
+            path(cur)
+            return
+
+        for i in (cur + 1, cur - 1, 2 * cur):
+            if 0 <= i < 100001 and dp[i] == 0:
+                q.append(i)
+                dp[i] = dp[cur] + 1
+                move[i] = cur
+
+
+bfs()
